0d7bc54aecafc96baaeff6f2481bd58c1dc10486
daxnet <daxnet@outlook.com>
2020-02-28 17:32:28 +0800

    Initial create.

---
+++ OcelotDemo.Gateway/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+
+namespace OcelotDemo.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                    configBuilder.AddJsonFile("ocelot.config.json");
+                })
+                .ConfigureServices((buildContext, services) =>
+                {
+                    services.AddOcelot();
+                });
+    }
+}
